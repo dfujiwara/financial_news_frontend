bab08eb670e0d2501dc86625cf5953825bd2788b
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-02-14 21:48:25 -0800

    Updates

---
+++ precache-manifest.48c0a9452f396fbf9bf3faf1bf54ca3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae67965ebf96e894bc60e059c7cba25c",
+    "url": "/financial_news_frontend/index.html"
+  },
+  {
+    "revision": "560a2560c459c87653dd",
+    "url": "/financial_news_frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2f5dab62372cf4a0dd96",
+    "url": "/financial_news_frontend/static/js/2.9d34efe7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/financial_news_frontend/static/js/2.9d34efe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560a2560c459c87653dd",
+    "url": "/financial_news_frontend/static/js/main.4cd6da98.chunk.js"
+  },
+  {
+    "revision": "0ab8484b5fe8894b06fc",
+    "url": "/financial_news_frontend/static/js/runtime-main.6983d207.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/financial_news_frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
