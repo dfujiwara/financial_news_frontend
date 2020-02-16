06ed61282d081bc31d035665b12ab934eb7bb072
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-02-16 11:01:00 -0800

    Add the network call to receive the data from the cloud function

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "https://us-central1-df-side-projects.cloudfunctions.net/news-rss-http"
+
+class App extends React.Component<{}> {
+  async componentDidMount() {
+    try {
+      const response = await fetch(url)
+      console.log(response)
+    } catch(e) {
+      console.log(`error happened: ${e}`)
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+            Hello world! YYOY!!!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
