e4234e2f1343089d416afed41dbce8f46a640857
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-03-22 20:16:48 -0700

    Set up linting for tsx files (#24)
    * Set up the right linting for tsx files
    
    * fixup! Add some top margin for the circular loader (#23)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,20 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
         sourceType: 'module', // Allows for the use of imports
+        ecmaFeatures: {
+            jsx: true, // Allows for the parsing of JSX
+        },
+    },
+    settings: {
+        react: {
+            createClass: 'createReactClass', // Regex for Component Factory to use,
+            // default to "createReactClass"
+            pragma: 'React', // Pragma to use, default to "React"
+            version: 'detect', // React version. "detect" automatically picks the version you have installed.
+            // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+            // default to latest and warns if missing
+            // It will default to "detect" in the future
+        },
     },
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
